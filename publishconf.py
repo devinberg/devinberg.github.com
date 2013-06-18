3df52c719a6e1f85ef58cc08d1f0fc60a656a396
devinberg <devinberg@gmail.com>
2013-06-18 14:19:36 -0500

    Updated to new tracking code.

--- publishconf.py
+++ publishconf.py
@@ -13,4 +13,4 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Uncomment following line for absolute URLs in production:
 #RELATIVE_URLS = False
-GOOGLE_ANALYTICS = "UA-3883459-1"
+GOOGLE_ANALYTICS = "UA-3883459-2"
